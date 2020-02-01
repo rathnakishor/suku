08fc3f3574f26205da2e2a3e25ed33922684260d
rathnakishor <59354054+rathnakishor@users.noreply.github.com>
2020-02-01 11:36:39 +0530

    Update ravi.java

--- src/main/java/project77/ravi.java
+++ src/main/java/project77/ravi.java
@@ -1,19 +1,8 @@
 package project77;
-import java.util.Scanner;
-public class ravi
-{
-    public static void main(String args[]) 
-    {
-        float p, r, t, sinterest;
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the Principal : ");
-        p = scan.nextFloat();
-        System.out.print("Enter the Rate of interest : ");
-        r = scan.nextFloat();
-        System.out.print("Enter the Time period : ");
-        t = scan.nextFloat();
-        scan.close();
-        sinterest = (p * r * t) / 100;
-        System.out.print("Simple Interest is: " +sinterest);
-    }
+public class ravi {
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println(name);
+  }
 }
+
